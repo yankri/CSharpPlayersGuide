a2bded7bc965bd8b4b57b0c31b6e38bc3cdad1f0
Krista Scholdberg <krista.scholdberg@gmail.com>
2015-12-31 00:25:04 -0500

    add new files

---
+++ Chapter 10 Even or Odd/ConsoleApplication2/EvenOrOdd Try it Out.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user for a number. Convert the number to an integer.
+            Console.WriteLine("Please enter an integer. This program will determine if it's even or odd.");
+            string userNumberInput = Console.ReadLine();
+            int userNumberQuery = Convert.ToInt32(userNumberInput); // Initiall
+
+            if (userNumberQuery == 0)
+            {
+                Console.WriteLine("You entered zero!");
+                // Zero is neither even nor odd, so I added a check for that. 
+            }
+            else
+            {
+                if (userNumberQuery % 2 == 0) //Checks for even. Even numbers will have no remainder.
+                {
+                    Console.WriteLine("Your number is even!");
+                }
+                else //Odd numbers will have a remainder. 
+                {
+                    Console.WriteLine("Your number is odd!");                        
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Chapter 10 Positive or Negative/Chapter 10 Positive or Negative/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_10_Positive_or_Negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You will be asked to enter two numbers. They can be positive or negative. This program will determine if their product is positive or negative.");
+
+            Console.WriteLine("Please enter the first number."); //First number input, converting to a double to handle decimals too. 
+            string userInput1 = Console.ReadLine();
+            double first = Convert.ToDouble(userInput1);
+
+            Console.WriteLine("Please enter the second number."); //Second number input. 
+            string userInput2 = Console.ReadLine();
+            double second = Convert.ToDouble(userInput2);
+
+            if (first == 0 || second == 0)   //If either number is 0, the answer will be zero, so I checked for that first. 
+            {
+                Console.WriteLine("Your answer will be zero.");
+            }
+
+            else
+            {
+                if ( (first < 0 && second < 0) || (first > 0 && second > 0) )  //Checking to see if the answer will be positive first. First part checks for two negative numbers, second checks for 2 positive numbers. Either result would be positive.
+                {
+                    Console.WriteLine("Your answer will be positive.");
+                }
+                else
+                {
+                    Console.WriteLine("Your answer will be negative."); // If one of the numbers is negative and the other is not, it will be negative. 
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
