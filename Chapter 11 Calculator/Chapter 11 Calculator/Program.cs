fb28dc4c2f872d9795e395e99aef9559e0cb07d9
Krista Scholdberg <krista.scholdberg@gmail.com>
2016-01-02 02:48:52 -0500

    forgot some stuff

---
+++ Chapter 11 Calculator/Chapter 11 Calculator/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_11_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("So you need to do some math, huh? \nEnter: \n+ for addition \n- for subtraction \n* for multiplication \n/ for division \n^ for exponentials");
+            var menuChoice = Console.ReadLine();
+
+            Console.WriteLine("Enter the first number you wish to use in your calculation. For exponents, x in x^y.");
+            string firstInput = Console.ReadLine();
+            double first = Convert.ToDouble(firstInput);
+
+            Console.WriteLine("Enter the second number you wish to use in your calculation. For exponents, y in x^y.");
+            string secondInput = Console.ReadLine();
+            double second = Convert.ToDouble(secondInput);
+
+            double result = 0.0;
+            bool showResult = true;
+
+            switch (menuChoice)
+            {
+                case "+":
+                    Console.WriteLine("You want to add the numbers " + first + " and " + second + ".");
+                    result = (first + second);
+                    break;
+                    
+                case "-":
+                    Console.WriteLine("You want to subtract the numbers " + first + " and " + second + ".");
+                    result = (first - second);
+                    break;
+
+                case "*":
+                    Console.WriteLine("You want to multiply the numbers " + first + " and " + second + ".");
+                    result = (first * second);
+                    break;
+
+                case "/":
+                    Console.WriteLine("You want to divide the numbers " + first + " and " + second + ".");
+                    result = (first / second);
+                    break;
+
+                case "^":
+                    Console.WriteLine("You want to multiply " + first + " by itself " + second + " times.");
+                    result = Math.Pow(first, second);
+                    break;
+
+                default:
+                    Console.WriteLine("Uh, did you enter an operator?");
+                    showResult = false;
+                    break;
+            }
+
+            if (showResult)
+            {
+                Console.WriteLine("The answer is " + result + ".");
+            }
+            
+            Console.ReadKey();
+
+            }
+
+
+        }
+    }
+
---
+++ Chapter 12 Take 2/Chapter 12 Take 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_12_Take_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+
+            //Prints out a pyramid of asterisks
+            for (int row = 1; row < 6; row++)
+            {
+                for (int space = 5-row; space > 0; space--)
+                {
+                    Console.Write("-"); //added dashes to make spaces visible
+                }
+                for (int column = 1; column < row * 2; column++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
