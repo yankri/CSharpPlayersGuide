286c530044941464c567def1f8e0cc600a7cea87
Krista Scholdberg <krista.scholdberg@gmail.com>
2016-02-09 10:56:29 -0500

    addd
    stuff

--- Chapter 15 Fibonacci/Chapter 15 Fibonacci/Program.cs
+++ Chapter 15 Fibonacci/Chapter 15 Fibonacci/Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-
 namespace Chapter_15_Fibonacci
 {
     class Program
     {
+        static int F(int number )
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a number");
-            int number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(Fibonacci(number));
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(F(number));
         }
 
-        static int Fibonacci(int number, int a = 1, int b = 1, int counter = 1)
+        static int F(int number, int a = 1, int b = 1, int counter = 1)
         {
             //Establish the base case
             if(number == 1)
@@ -24,7 +24,7 @@ namespace Chapter_15_Fibonacci
             }
             if (counter < number)
             {
-                return Fibonacci(number, b, a + b, counter + 1); //counter+1 is the increment
+                return F(number, b, a + b, counter + 1); //counter+1 is the increment
             }
             else //else is always the last function call in the sequence, the end of the recursion (in nesting doll analogy, the last doll you said it could use)
             {
